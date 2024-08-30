b84e03e8bb729459c6f26339ae1531d29a309878
Aleksy <aleksy.geborek@gmail.com>
2024-08-30 22:19:02 +0200

    javascript update

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ function manipulateParagraph1() {
 	if (!element.style.display || element.style.display === 'none') {
 		element.style.display = 'block';
 		console.log(element.style.display);
-		console.log(1);
 	} else if (element.style.display === 'block') {
 		element.style.display = 'none';
 		console.log(element.style.display);
-		console.log(2);
 	} else {
 		console.log(element.style.display);
 		console.log('zjebales');
